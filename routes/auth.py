02c63d4c4eeaf0cf1b3ef08a47830149850219e0
Vladimir Dybin <vladimir.dybin@masterdelivery.ru>
2025-01-13 12:42:06 +0300

    feature: yd_auth

--- routes/auth.py
+++ routes/auth.py
@@ -55,3 +55,25 @@ async def google_auth(
         code: str
 ):
     return auth_service.google_auth(code=code)
+
+
+@router.get(
+    '/login/yandex',
+    response_class=RedirectResponse
+)
+async def yandex_login(
+        auth_service: Annotated[AuthService, Depends(get_auth_service)]
+):
+    redirect_url = auth_service.get_yandex_redirect_url()
+    print(redirect_url)
+    return redirect_url
+
+
+@router.get(
+    '/yandex'
+)
+async def yandex_auth(
+        code:str,
+        auth_service: Annotated[AuthService, Depends(get_auth_service)]
+):
+    return auth_service.yandex_auth(code=code)
--- settings.py
+++ settings.py
@@ -17,6 +17,10 @@ class Settings(BaseSettings):
     GOOGLE_SECRET_KEY: str = ''
     GOOGLE_REDIRECT_URI: str = ''
     GOOGLE_TOKEN_URL: str = 'https://accounts.google.com/o/oauth2/token'
+    YANDEX_CLIENT_ID: str = ''
+    YANDEX_SECRET_KEY: str = ''
+    YANDEX_REDIRECT_URI: str = ''
+    YANDEX_TOKEN_URL: str = 'https://oauth.yandex.ru/token'
 
     @property
     def db_url(self):
@@ -25,4 +29,8 @@ class Settings(BaseSettings):
     @property
     def google_redirect_url(self) -> str:
         return f'https://accounts.google.com/o/oauth2/auth?response_type=code&client_id={self.GOOGLE_CLIENT_ID}&redirect_uri={self.GOOGLE_REDIRECT_URI}&scope=openid%20profile%20email&access_type=offline'
+
+    @property
+    def yandex_redirect_url(self) -> str:
+        return f'https://oauth.yandex.ru/authorize?response_type=code&client_id={self.YANDEX_CLIENT_ID}&redirect_uri={self.YANDEX_REDIRECT_URI}'
 
